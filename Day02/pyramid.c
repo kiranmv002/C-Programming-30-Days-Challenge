269a422ff3638c4e0fa50b13c2375e986231e9e5
M V Kiran <mada.kiran@s.amity.edu>
2026-02-17 22:33:34 +0530

    Update pyramid.c

--- Day02/pyramid.c
+++ Day02/pyramid.c
@@ -1,1 +1,21 @@
+int main() {
+    int n;
 
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++) {
+
+        for(int space = 1; space <= n - i; space++) {
+            printf(" ");
+        }
+
+        for(int star = 1; star <= (2*i - 1); star++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
