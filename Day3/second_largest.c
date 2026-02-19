68721921084b0a9e8c8411a6366b4f876bbb2e80
M V Kiran <mada.kiran@s.amity.edu>
2026-02-19 20:37:18 +0530

    Create second_largest.c

---
+++ Day3/second_largest.c
@@ -0,0 +1,28 @@
+int main() {
+    int n;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int largest = arr[0], second = arr[0];
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > largest) {
+            second = largest;
+            largest = arr[i];
+        } else if(arr[i] > second && arr[i] != largest) {
+            second = arr[i];
+        }
+    }
+
+    printf("Second largest element = %d\n", second);
+
+    return 0;
+}
