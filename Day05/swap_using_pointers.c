1a120cf17cc13dadf77fe1f64fe1c52ed0a503eb
kiranmv002 <mada.kiran@s.amity.edu>
2026-02-24 21:23:14 +0530

    Intial Commit

---
+++ Day05/swap_using_pointers.c
@@ -0,0 +1,18 @@
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x, y;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    swap(&x, &y);
+
+    printf("After swapping: %d %d\n", x, y);
+
+    return 0;
+}
