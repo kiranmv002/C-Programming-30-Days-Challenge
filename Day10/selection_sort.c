d5ea05bb6b1e8de9d63684d86f3422d0fd119394
M V Kiran <mada.kiran@s.amity.edu>
2026-03-15 22:53:05 +0530

    Create selection_sort.c

---
+++ Day10/selection_sort.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
