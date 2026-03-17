446a78882f926683dd162c812d1ab8922243fb25
M V Kiran <mada.kiran@s.amity.edu>
2026-03-17 19:26:41 +0530

    Add display_array.c to print array elements

---
+++ Day10/display_array.c
@@ -0,0 +1,13 @@
+int main() {
+
+    int arr[] = {10, 5, 20, 3, 15};
+    int n = 5;
+
+    printf("Array elements:\n");
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
