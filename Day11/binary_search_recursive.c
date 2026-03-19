98880fce37c8323ed5e6df455d07b1e256ceebca
M V Kiran <mada.kiran@s.amity.edu>
2026-03-19 19:58:34 +0530

    Create binary_search_recursive.c

---
+++ Day11/binary_search_recursive.c
@@ -0,0 +1,36 @@
+int binarySearch(int arr[], int low, int high, int key) {
+
+    if(low > high)
+        return -1;
+
+    int mid = (low + high) / 2;
+
+    if(arr[mid] == key)
+        return mid;
+    else if(arr[mid] < key)
+        return binarySearch(arr, mid + 1, high, key);
+    else
+        return binarySearch(arr, low, mid - 1, key);
+}
+
+int main() {
+    int n, key;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter sorted elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    int result = binarySearch(arr, 0, n - 1, key);
+
+    if(result != -1)
+        printf("Element found at position %d\n", result + 1);
+    else
