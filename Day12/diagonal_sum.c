404d31cfc216804c2b6402373fb7235ec6ddc0e3
M V Kiran <mada.kiran@s.amity.edu>
2026-03-22 22:23:51 +0530

    Update diagonal_sum.c

--- Day12/diagonal_sum.c
+++ Day12/diagonal_sum.c
@@ -1,3 +1,17 @@
 
 int main() {
     int matrix[3][3], sum = 0;
+
+    printf("Enter 3x3 matrix:\n");
+    for(int i=0;i<3;i++)
+        for(int j=0;j<3;j++)
+            scanf("%d",&matrix[i][j]);
+
+    for(int i=0;i<3;i++) {
+        sum += matrix[i][i];  // main diagonal
+    }
+
+    printf("Sum of diagonal = %d\n", sum);
+
+    return 0;
+}
