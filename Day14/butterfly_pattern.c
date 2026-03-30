a85ce6f7d1e24e4fb1fa2beb50063c28f6719cea
M V Kiran <mada.kiran@s.amity.edu>
2026-03-30 18:03:23 +0530

    Update butterfly_pattern.c

--- Day14/butterfly_pattern.c
+++ Day14/butterfly_pattern.c
@@ -19,3 +19,22 @@ int main() {
             printf("*");
 
         printf("\n");
+    }
+
+    // Lower part
+    for(int i = n; i >= 1; i--) {
+
+        for(int j = 1; j <= i; j++)
+            printf("*");
+
+        for(int space = 1; space <= 2*(n - i); space++)
+            printf(" ");
+
+        for(int j = 1; j <= i; j++)
+            printf("*");
+
+        printf("\n");
+    }
+
+    return 0;
+}
