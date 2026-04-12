fb5b02e01e2c5c2914766fe37555625102d617e7
M V Kiran <mada.kiran@s.amity.edu>
2026-04-12 20:30:45 +0530

    Update append_records.c

--- Day17/append_records.c
+++ Day17/append_records.c
@@ -6,3 +6,20 @@ struct Student {
     float marks;
 };
 
+int main() {
+    FILE *fp;
+    struct Student s;
+
+    fp = fopen("students.txt", "a");
+
+    printf("Enter roll, name, marks to append: ");
+    scanf("%d %s %f", &s.roll, s.name, &s.marks);
+
+    fprintf(fp, "%d %s %.2f\n", s.roll, s.name, s.marks);
+
+    fclose(fp);
+
+    printf("Record appended successfully.\n");
+
+    return 0;
+}
