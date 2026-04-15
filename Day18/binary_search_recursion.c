0b1e6e0a9dbc2a34b12c8172e1441022229bbe7d
M V Kiran <mada.kiran@s.amity.edu>
2026-04-15 23:11:41 +0530

    Add binary search implementation using recursion

---
+++ Day18/binary_search_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int low, int high, int key) {
+
+    if(low > high)
+        return -1;
+
+    int mid = (low + high) / 2;
+
+    if(arr[mid] == key)
+        return mid;
+    else if(arr[mid] < key)
+        return binarySearch(arr, mid + 1, high, key);
+    else
+        return binarySearch(arr, low, mid - 1, key);
+}
