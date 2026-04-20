2c30947b3fb74006bb53ec38a0116e94317b17b1
M V Kiran <mada.kiran@s.amity.edu>
2026-04-20 23:28:10 +0530

    Update realloc_example.c

--- Day19/realloc_example.c
+++ Day19/realloc_example.c
@@ -14,3 +14,23 @@ int main() {
         scanf("%d", &ptr[i]);
     }
 
+    int newSize;
+    printf("Enter new size: ");
+    scanf("%d", &newSize);
+
+    ptr = (int*) realloc(ptr, newSize * sizeof(int));
+
+    printf("Enter new elements:\n");
+    for(int i = n; i < newSize; i++) {
+        scanf("%d", &ptr[i]);
+    }
+
+    printf("Updated array:\n");
+    for(int i = 0; i < newSize; i++) {
+        printf("%d ", ptr[i]);
+    }
+
+    free(ptr);
+
+    return 0;
+}
