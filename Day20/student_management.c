cab71178c6f778a1e062b892d77f8cb897a936f8
M V Kiran <mada.kiran@s.amity.edu>
2026-04-24 21:59:24 +0530

    Update student_management.c

--- Day20/student_management.c
+++ Day20/student_management.c
@@ -54,3 +54,33 @@ void searchStudent() {
         }
     }
 
+    if(!found)
+        printf("Student not found.\n");
+
+    fclose(fp);
+}
+
+int main() {
+    int choice;
+
+    while(1) {
+        printf("\n--- Student Management System ---\n");
+        printf("1. Add Student\n");
+        printf("2. View Students\n");
+        printf("3. Search Student\n");
+        printf("4. Exit\n");
+
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1: addStudent(); break;
+            case 2: viewStudents(); break;
+            case 3: searchStudent(); break;
+            case 4: exit(0);
+            default: printf("Invalid choice\n");
+        }
+    }
+
+    return 0;
+}
