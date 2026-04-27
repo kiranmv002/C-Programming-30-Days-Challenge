c51027fc66b05bb37bb1b085a25bc98331f4e60e
M V Kiran <mada.kiran@s.amity.edu>
2026-04-27 22:05:12 +0530

    Create reverse_linked_list.c

---
+++ Day21/reverse_linked_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+
+    struct Node *head = NULL, *prev = NULL, *current, *next;
+
+    struct Node *n1 = malloc(sizeof(struct Node));
+    struct Node *n2 = malloc(sizeof(struct Node));
+    struct Node *n3 = malloc(sizeof(struct Node));
+
+    n1->data = 10; n2->data = 20; n3->data = 30;
+    n1->next = n2; n2->next = n3; n3->next = NULL;
+
+    head = n1;
+
+    current = head;
+
