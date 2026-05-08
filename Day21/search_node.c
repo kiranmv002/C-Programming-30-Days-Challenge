0a21bcf9b6b45de3fe193eabecd4eefbe3c70333
M V Kiran <mada.kiran@s.amity.edu>
2026-05-08 22:01:13 +0530

    Create search_node.c

---
+++ Day21/search_node.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+
+    struct Node *n1 = malloc(sizeof(struct Node));
+    struct Node *n2 = malloc(sizeof(struct Node));
+    struct Node *n3 = malloc(sizeof(struct Node));
+
+    n1->data = 10; n2->data = 20; n3->data = 30;
+    n1->next = n2; n2->next = n3; n3->next = NULL;
+
+    struct Node *temp = n1;
+    int key = 20, found = 0;
+
+    while(temp != NULL) {
+        if(temp->data == key) {
+            found = 1;
+            break;
+        }
+        temp = temp->next;
+    }
