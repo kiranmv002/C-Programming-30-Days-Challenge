503a64d4990ff9ff82210cff26bfb537f3159ac3
M V Kiran <mada.kiran@s.amity.edu>
2026-06-03 22:26:26 +0530

    Create insert_beginning_dll.c

---
+++ Day23/insert_beginning_dll.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *prev;
+    struct Node *next;
+};
+
+int main() {
+
+    struct Node *head = NULL;
+
+    struct Node *newNode = malloc(sizeof(struct Node));
+
+    printf("Enter value: ");
+    scanf("%d", &newNode->data);
+
+    newNode->prev = NULL;
+    newNode->next = head;
