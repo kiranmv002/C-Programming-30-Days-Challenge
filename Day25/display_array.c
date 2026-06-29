ac13ee0f0663ddcd5e514d6dbe4c79bc5a7910d4
M V Kiran <mada.kiran@s.amity.edu>
2026-06-29 20:42:47 +0530

    Create display_array.c

---
+++ Day25/display_array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int arr[] = {5, 2, 8, 1, 9};
+
+    printf("Array Elements:\n");
+
+    for(int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
