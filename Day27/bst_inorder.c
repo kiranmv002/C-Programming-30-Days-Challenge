d0be8ac8caf86e292041e7070af37547075624ef
M V Kiran <mada.kiran@s.amity.edu>
2026-07-04 19:11:52 +0530

    Update bst_inorder.c

--- Day27/bst_inorder.c
+++ Day27/bst_inorder.c
@@ -16,3 +16,17 @@ void inorder(struct Node *root) {
     printf("%d ", root->data);
     inorder(root->right);
 }
+
+int main() {
+
+    struct Node n1 = {40, NULL, NULL};
+    struct Node n2 = {20, NULL, NULL};
+    struct Node n3 = {60, NULL, NULL};
+
+    n1.left = &n2;
+    n1.right = &n3;
+
+    inorder(&n1);
+
+    return 0;
+}
