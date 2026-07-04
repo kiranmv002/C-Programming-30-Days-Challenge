95889b6ed9f7da48f9e5a0835aaf5bde0049eb74
M V Kiran <mada.kiran@s.amity.edu>
2026-07-04 19:11:33 +0530

    Update bst_insert.c

--- Day27/bst_insert.c
+++ Day27/bst_insert.c
@@ -27,3 +27,28 @@ struct Node* insert(struct Node *root, int value) {
 
     return root;
 }
+
+void inorder(struct Node *root) {
+
+    if(root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+int main() {
+
+    struct Node *root = NULL;
+
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 20);
+    insert(root, 40);
+
+    printf("Inorder Traversal:\n");
+    inorder(root);
+
+    return 0;
+}
