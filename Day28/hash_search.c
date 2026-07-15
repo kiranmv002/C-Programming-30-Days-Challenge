2fe3d0e2f13e2c186eafcb48275829ea43cd6e86
M V Kiran <mada.kiran@s.amity.edu>
2026-07-15 20:40:06 +0530

    Create hash_search.c

---
+++ Day28/hash_search.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define SIZE 10
+
+int hashTable[SIZE] = {-1,-1,-1,-1,-1,15,25,37,-1,-1};
+
+int main() {
+
+    int key;
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
