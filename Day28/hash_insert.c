530d28903e95845cd4981a3bfbff3db86e8ce03d
M V Kiran <mada.kiran@s.amity.edu>
2026-07-15 20:39:33 +0530

    Create hash_insert.c

---
+++ Day28/hash_insert.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define SIZE 10
+
+int hashTable[SIZE];
+
+void initialize() {
+    for(int i = 0; i < SIZE; i++)
+        hashTable[i] = -1;
+}
+
+void insert(int value) {
+    int index = value % SIZE;
+
+    if(hashTable[index] == -1)
+        hashTable[index] = value;
+    else
+        printf("Collision occurred for %d\n", value);
+}
+
+int main() {
+
+    initialize();
+
+    insert(15);
+    insert(25);
+    insert(37);
+
+    printf("Elements inserted.\n");
+
+    return 0;
+}
