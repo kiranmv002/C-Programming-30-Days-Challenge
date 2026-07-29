ce33744fa630b452893038193e858091c0047bea
M V Kiran <mada.kiran@s.amity.edu>
2026-07-29 23:16:27 +0530

    Update library_management.c

--- Day29/library_management.c
+++ Day29/library_management.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+-#include <stdio.h>
 #include <stdlib.h>
 
 struct Book {
@@ -30,5 +30,55 @@ void addBook() {
     fclose(fp);
 
     printf("Book added successfully.\n");
+}
+
+void viewBooks() {
+    FILE *fp = fopen("books.txt", "r");
+    struct Book b;
+
+    if(fp == NULL) {
+        printf("No books available.\n");
+        return;
+    }
+
+    printf("\n----- Book List -----\n");
+
+    while(fscanf(fp, "%d,%49[^,],%49[^\n]\n", &b.id, b.title, b.author) == 3) {
+        printf("ID     : %d\n", b.id);
+        printf("Title  : %s\n", b.title);
+        printf("Author : %s\n\n", b.author);
+    }
+
+    fclose(fp);
+}
+
+void searchBook() {
+    FILE *fp = fopen("books.txt", "r");
+    struct Book b;
+    int id, found = 0;
+
+    if(fp == NULL) {
+        printf("No books available.\n");
+        return;
+    }
+
+    printf("Enter Book ID to search: ");
+    scanf("%d", &id);
+
+    while(fscanf(fp, "%d,%49[^,],%49[^\n]\n", &b.id, b.title, b.author) == 3) {
+        if(b.id == id) {
+            printf("\nBook Found\n");
+            printf("ID     : %d\n", b.id);
+            printf("Title  : %s\n", b.title);
+            printf("Author : %s\n", b.author);
+            found = 1;
+            break;
+        }
+    }
+
+    if(!found)
+        printf("Book not found.\n");
+
+    fclose(fp);
 }
 
